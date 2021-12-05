86e321752ab0e5e9b86477832a187f26c2f099f2
ii.terekhov <ii.terekhov@iiterekhov-nb.local>
2021-12-05 13:53:49 +0300

    Fix db name

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ $local = include '_db.php';
 
 return array_replace([
   'class'    => 'yii\db\Connection',
-  'dsn'      => 'mysql:host=localhost;dbname=instablocker',
+  'dsn'      => 'mysql:host=localhost;dbname=apelsin',
   'username' => 'username',
   'password' => 'password',
   'charset'  => 'utf8mb4',
